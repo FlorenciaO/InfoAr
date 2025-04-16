ca7e0eefb18d881bb44a359522cace6571300895
FlorenciaO <o.florencia123@gmail.com>
2025-04-16 18:56:24 -0300

    Fix import

--- app/src/main/java/com/educacionit/infoar/presentation/fragments/TermsAndConditionsFragment.kt
+++ app/src/main/java/com/educacionit/infoar/presentation/fragments/TermsAndConditionsFragment.kt
@@ -1,8 +1,4 @@
-<<<<<<<< HEAD:app/src/main/java/com/educacionit/infoar/presentacion/fragments/TermsAndConditionsFragment.kt
-package com.educacionit.infoar.presentacion.fragments
-========
 package com.educacionit.infoar.presentation.fragments
->>>>>>>> refs/heads/feature/apply-orm-lite-resolution:app/src/main/java/com/educacionit/infoar/presentation/fragments/TermsAndConditionsFragment.kt
 
 import android.os.Bundle
 import android.view.LayoutInflater
